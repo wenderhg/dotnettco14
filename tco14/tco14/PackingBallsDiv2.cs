f21dc88e13cf0728a9acfd984c1850671e633b83
Hooberth Wender Gomes <hgomes@ciandt.com>
2014-11-14 19:44:36 -0800

    first commit

---
+++ tco14/tco14/PackingBallsDiv2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace tco14
+{
+    public class PackingBallsDiv2
+    {
+        public int minPacks(int R, int G, int B)
+        {
+            if (R == G && G == B)
+                return R;
+
+            var colors = new List<int>() {R, G, B};
+            colors.Sort();
+            var sets = new List<int>();
+            
+            sets.Add(colors[0]);
+
+            var remainingSecondColor = calculeNumberofSets(colors[1] - colors[0], sets, 2);            
+            var remainingThirdColor = calculeNumberofSets(colors[2] - colors[0], sets, 2);
+
+            sets.Add(getNumberOfSets(remainingSecondColor, 1));
+            remainingThirdColor -= sets.Last();
+
+            sets.Add(getNumberOfSets(remainingThirdColor, 1));
+
+            return sets.Sum();
+        }
+
+        private int calculeNumberofSets(int color, List<int> sets, int limit = 1)
+        {
+            for (int i = 3; i > limit - 1; i--)
+            {
+                sets.Add(getNumberOfSets(color, i));
+                color -= sets.Last()*i;
+            }
+            return color;
+        }
+
+        private int getNumberOfSets(int color, int numberOfBalls = 3)
+        {
+            int result = 0;
+            while (color >= numberOfBalls)
+            {
+                color = color - numberOfBalls;
+                result++;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ tco14/tco14Test/PackingBallsDiv2Test.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tco14;
+
+namespace tco14Test
+{
+    [TestClass]
+    public class PackingBallsDiv2Test
+    {
+        PackingBallsDiv2 tester = new PackingBallsDiv2();
+
+        [TestMethod]
+        public void scenario_1()
+        {
+            int actual = tester.minPacks(4, 2, 4);
+            Assert.AreEqual(4, actual);
+        }
+
+        [TestMethod]
+        public void scenario_2()
+        {
+            int actual = tester.minPacks(1, 7, 1);
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void scenario_3()
+        {
+            int actual = tester.minPacks(2, 3, 5);
+            Assert.AreEqual(4, actual);
+        }
+
+        [TestMethod]
+        public void scenario_4()
+        {
+            int actual = tester.minPacks(78, 53, 64);
+            Assert.AreEqual(66, actual);
+        }
+
+        [TestMethod]
+        public void scenario_5()
+        {
+            int actual = tester.minPacks(100, 100, 100);
+            Assert.AreEqual(100, actual);
+        }
+
+        [TestMethod]
+        public void scenario_6()
+        {
+            int actual = tester.minPacks(1, 2, 2);
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void scenario_7()
+        {
+            int actual = tester.minPacks(1, 2, 5);
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void scenario_8()
+        {
+            int actual = tester.minPacks(1, 5, 5);
+            Assert.AreEqual(4, actual);
+        }
+        
+        [TestMethod]
+        public void scenario_9()
+        {
+            int actual = tester.minPacks(1, 4, 4);
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void scenario_10()
+        {
+            int actual = tester.minPacks(2, 3, 3);
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void scenario_11()
+        {
+            int actual = tester.minPacks(1, 3, 4);
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void scenario_12()
+        {
+            int actual = tester.minPacks(3, 3, 5);
+            Assert.AreEqual(4, actual);
+        }
+
+        [TestMethod]
+        public void scenario_13()
+        {
+            int actual = tester.minPacks(4, 5, 5);
+            Assert.AreEqual(5, actual);
+        }
+
+    }
+}
